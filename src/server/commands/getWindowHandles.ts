6298da78db99a20488a0d87a10f56749359ddadd
Corneliu Duplachi <cornel.duplachi@gmail.com>
2025-02-23 15:12:29 +0200

    fix: drop iterator helpers usage

--- src/server/commands/getWindowHandles.ts
+++ src/server/commands/getWindowHandles.ts
@@ -2,5 +2,5 @@ import type { HtmlDriver } from '../Driver.js';
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function getWindowHandles(this: HtmlDriver): Promise<string[]> {
-  return [...new Set(this.namespace.sockets.values().map((it) => it.data.handle))];
+  return [...new Set([...this.namespace.sockets.values()].map((it) => it.data.handle))];
 }
