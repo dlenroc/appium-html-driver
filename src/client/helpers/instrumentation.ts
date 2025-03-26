16c6d2d4a34ac8bedd4609211ebbd64b57e4aa98
Corneliu Duplachi <cornel.duplachi@gmail.com>
2025-03-27 00:20:14 +0200

    feat!: use UUID as fallback and escape params properly

--- src/client/helpers/instrumentation.ts
+++ src/client/helpers/instrumentation.ts
@@ -1,10 +1,27 @@
 import { URL as URLShim } from 'url-shim';
+import { v4 as uuid } from 'uuid';
 
-// @ts-expect-error - src can be undefined
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-export const INSTRUMENTATION_URL = new URLShim(document.currentScript?.src ?? '', location.href);
-export const INSTRUMENTATION_UDID = INSTRUMENTATION_URL.searchParams.get('udid')?.trim() || '<appium-html-driver-udid>';
-export const INSTRUMENTATION_HANDLE = INSTRUMENTATION_URL.searchParams.get('handle')?.trim() || '<appium-html-driver-handle>';
+declare global {
+  interface Window {
+    APPIUM_HTML_DRIVER_UDID?: string;
+    APPIUM_HTML_DRIVER_HANDLE?: string;
+  }
+}
+
+export const INSTRUMENTATION_URL = new URLShim(
+  document.currentScript?.getAttribute('src') ?? '',
+  location.href
+);
+
+export const INSTRUMENTATION_UDID =
+  INSTRUMENTATION_URL.searchParams.get('udid')?.trim() ||
+  window.APPIUM_HTML_DRIVER_UDID ||
+  uuid();
+
+export const INSTRUMENTATION_HANDLE =
+  INSTRUMENTATION_URL.searchParams.get('handle')?.trim() ||
+  window.APPIUM_HTML_DRIVER_HANDLE ||
+  uuid();
 
 export function getHomeUrl(handle: string): URLShim {
   const url = new URLShim('./home', INSTRUMENTATION_URL);
